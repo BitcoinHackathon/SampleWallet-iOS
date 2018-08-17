e15efab4b4b0f13105c6c630528167ac7cab8e1d
藤田哲史 <tessitetsusi@gmail.com>
2018-08-17 12:32:01 +0900

    :pencil2: add ()

--- SampleWallet/HomeViewController.swift
+++ SampleWallet/HomeViewController.swift
@@ -39,7 +39,7 @@ class HomeViewController: UITableViewController {
     
     private func updateUI() {
         getAddress()
-        //getBalance
+        //getBalance()
         //getTxHistory()
     }
     
